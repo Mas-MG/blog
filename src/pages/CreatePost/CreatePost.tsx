d37cd08f4da77db109afaaa9b35b4487c265ef41
masoomeh ghorbani <masoomeh77ghorbani98@gmail.com>
2025-04-13 09:42:56 +0330

    Add: View all posts completed!

--- src/pages/CreatePost/CreatePost.tsx
+++ src/pages/CreatePost/CreatePost.tsx
@@ -1,86 +1,103 @@
-import {FormEvent, useState } from "react";
+import { FormEvent, useEffect, useState } from "react";
 import Sidebar from "../../components/Sidebar";
 import usePostStore from "../../store/PostStore";
-import { useMutation} from "@tanstack/react-query";
+import { useMutation } from "@tanstack/react-query";
 import { BeatLoader } from "react-spinners";
 import toast from "react-hot-toast";
 import { useNavigate } from "react-router";
 
-export interface Post{
-    userId:string,
-    id:string,
-    title:string,
-    body:string
+export interface Post {
+  userId: string;
+  id: string;
+  title: string;
+  body: string;
 }
 
 const CreatePost = () => {
+  const created = usePostStore((state) => state.created);
+  const newPost = usePostStore((state) => state.newPost);
+  const setActive = usePostStore((state) => state.setActive);
+  
+  useEffect(() => {
+    localStorage.removeItem("activeListitem");
+    setActive("create");
+  }, []);
+  const navigate = useNavigate();
 
-    const created=usePostStore(state=>state.created)
-    const newPost=usePostStore(state=>state.newPost)
-    const setActive=usePostStore(state=>state.setActive)
-    const navigate=useNavigate()
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
 
-    const [title, setTitle] = useState("");
-    const [description, setDescription] = useState("");
+  const createPost = async (formData: {
+    userId: string;
+    title: string;
+    body: string;
+  }) => {
+    const res = await fetch("https://jsonplaceholder.typicode.com/posts", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(formData),
+    });
+    return await res.json();
+  };
 
-    const createPost=async(formData:{userId:string,title:string,body:string})=>{
-            const res=await fetch("https://jsonplaceholder.typicode.com/posts",{
-                method:"POST",
-                headers:{
-                    "content-type":"application/json"
-                },
-                body:JSON.stringify(formData)
-            })
-            return await res.json()
-        }
-
-    const {mutate,isPending}=useMutation({
-        mutationFn:createPost,
-        onSuccess:(data)=>{
-         created()
-         newPost(data)
-         toast.success("Post successfully created! Now you have access to all posts.")
-         navigate('/step/2')
-         setActive("view")
-        },
-        onError:()=>{
-        toast.error("Something went wrong!")
-        }
-    })
-    const handleSubmit=(e:FormEvent)=>{
-        e.preventDefault()
-        if(!title){
-            toast.error("Title is required!")
-        }else if(!description){
-            toast.error("description is required")
-        }else{
-            mutate({"userId":"1",title,"body":description})
-        }
+  const { mutate, isPending } = useMutation({
+    mutationFn: createPost,
+    onSuccess: (data) => {
+      created();
+      newPost(data);
+      toast.success(
+        "Post successfully created! Now you have access to all posts."
+      );
+      navigate("/step/2");
+      setActive("view");
+    },
+    onError: (error) => {
+      toast.error(error?.message);
+    },
+  });
+  const handleSubmit = (e: FormEvent) => {
+    e.preventDefault();
+    if (!title) {
+      toast.error("Title is required!");
+    } else if (!description) {
+      toast.error("description is required");
+    } else {
+      mutate({ userId: "1", title, body: description });
     }
+  };
 
   return (
     <div className="main">
-    <div className="main-container create">
-      <Sidebar />
-      <div className="content">
-        <h1 className="title">Create Post</h1>
-        <form onSubmit={handleSubmit} className="form">
+      <div className="main-container create">
+        <Sidebar />
+        <div className="content">
+          <h1 className="title">Create Post</h1>
+          <form onSubmit={handleSubmit} className="form">
             <div className="form-post-title">
-                <label>
-                    Title:
-                    <input onChange={(e)=>setTitle(e?.target?.value)} type="text"/>
-                </label>
+              <label>
+                Title:
+                <input
+                  onChange={(e) => setTitle(e?.target?.value)}
+                  type="text"
+                />
+              </label>
             </div>
             <div className="form-post-description">
-                <label>
-                    Description:
-                    <textarea onChange={(e)=>setDescription(e?.target?.value)}></textarea>
-                </label>
+              <label>
+                Description:
+                <textarea
+                  onChange={(e) => setDescription(e?.target?.value)}
+                ></textarea>
+              </label>
             </div>
-            <button type="submit" className="submit">{isPending ? <BeatLoader size={10}/> : "Submit"}</button>
-        </form>
+            <button type="submit" className="submit">
+              {isPending ? <BeatLoader size={10} /> : "Submit"}
+            </button>
+          </form>
+        </div>
       </div>
-    </div>
     </div>
   );
 };
--- src/pages/ViewPosts/ViewPosts.tsx
+++ src/pages/ViewPosts/ViewPosts.tsx
@@ -2,13 +2,15 @@ import { useQuery } from "@tanstack/react-query";
 import Sidebar from "../../components/Sidebar";
 import { Post } from "../CreatePost/CreatePost";
 import PostCard from "./PostCard/PostCard";
+import { GridLoader } from "react-spinners";
+import toast from "react-hot-toast";
 
 const ViewPosts = () => {
   const allPosts = async () => {
     const res = await fetch("https://jsonplaceholder.typicode.com/posts");
     return await res.json();
   };
-  const { data, isPending, isSuccess } = useQuery({
+  const { data, isPending, isError,error} = useQuery({
     queryKey: ["posts"],
     queryFn: allPosts,
   });
@@ -17,17 +19,19 @@ const ViewPosts = () => {
     <PostCard key={post?.id} post={post} />
   ));
 
+  if(isError) toast.error(error?.message)
+
   return (
     <div className="main">
-    <div className="main-container view">
-      <Sidebar />
-      <div className="content">
-        <h1 className="title">View All Posts</h1>
-        <div className="posts">
-          {posts}
+      <div className="main-container view">
+        <Sidebar />
+        <div className="content">
+          <h1 className="title">View All Posts</h1>
+          <div className={`posts ${isPending ? "items-center justify-center" : ""}`}>
+          {isPending ?<GridLoader /> :isError ? error?.message : posts  }
+          </div>
         </div>
       </div>
-    </div>
     </div>
   );
 };
